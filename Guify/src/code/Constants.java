7c9db6b5ba18c288a39cb5771736aa8435cf2fa1
xfarrow <ale99napoli@icloud.com>
2023-07-24 15:10:12 +0200

    1.0.2

--- Guify/src/code/Constants.java
+++ Guify/src/code/Constants.java
@@ -4,8 +4,8 @@ import java.awt.Color;
 
 public class Constants {
 	public static final String APP_NAME = "Guify";
-	public static final String VERSION = "1.0.1";
-	public static final int VERSION_PROGRESSIVE = 1;
+	public static final String VERSION = "1.0.2";
+	public static final int VERSION_PROGRESSIVE = 2;
 	
 	public static class Constants_FSOperations{
 		public static final int NONE = 0;
